d9b2cc1c463d59dab3c9d627bc0c5d8e2f475a8c
chspereira97 <chs.pereira97@gmail.com>
2026-05-21 14:31:40 -0300

    Segurança: SECRET_KEY movida para .env

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente
+load_dotenv()
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'instance', 'sistema.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, '..', 'instance', 'sistema.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'chave-secreta-temporaria'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'chave-padrao-insegura')
 
 db = SQLAlchemy(app)
 
@@ -21,7 +25,6 @@ login_manager.login_message_category = 'warning'
 
 app.jinja_env.globals['timedelta'] = timedelta
 
-# Proteção global de rotas
 @app.before_request
 def require_login():
     public_endpoints = ['login', 'static']
@@ -29,13 +32,11 @@ def require_login():
         if request.endpoint not in public_endpoints:
             return redirect(url_for('login'))
 
-# Importar models
 from app.models import Aluno, Responsavel, aluno_responsavel, Projeto, Turma, Matricula, Frequencia, Usuario, professor_turma
 
 @login_manager.user_loader
 def load_user(user_id):
     return Usuario.query.get(int(user_id))
 
-# Importar rotas
 from app import routes
 from app.controllers import aluno_controller, responsavel_controller, turma_controller, matricula_controller, frequencia_controller, auth_controller, relatorio_controller
